343326bf8e8c17064ee1c85a4b979d65d3ac3ec7
brijamfitz <brianjfitzgerald@gmail.com>
2018-12-19 20:44:57 -0500

    Updates

---
+++ precache-manifest.39e0c549890cf1ca98c34aebfb082b80.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "51df0c368b010d1085d4",
+    "url": "/Simpsons-Clicky/static/js/runtime~main.51df0c36.js"
+  },
+  {
+    "revision": "27975060fbefe262fa40",
+    "url": "/Simpsons-Clicky/static/js/main.27975060.chunk.js"
+  },
+  {
+    "revision": "841a72d92bddb8760d2d",
+    "url": "/Simpsons-Clicky/static/js/1.841a72d9.chunk.js"
+  },
+  {
+    "revision": "27975060fbefe262fa40",
+    "url": "/Simpsons-Clicky/static/css/main.9609d3b4.chunk.css"
+  },
+  {
+    "revision": "13215b2342eba9cd7faea64e9cafdaa3",
+    "url": "/Simpsons-Clicky/index.html"
+  }
+];

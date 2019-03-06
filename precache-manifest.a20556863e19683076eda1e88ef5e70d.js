e8f96157920c2f4114203d41bfbd21e60e9e3ab6
brijamfitz <brianjfitzgerald@gmail.com>
2019-03-05 23:11:12 -0500

    Updates

--- precache-manifest.a20556863e19683076eda1e88ef5e70d.js
+++ precache-manifest.a20556863e19683076eda1e88ef5e70d.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Simpsons-Clicky/static/js/runtime~main.51df0c36.js"
   },
   {
-    "revision": "c7c5e6349adc34c9583c",
-    "url": "/Simpsons-Clicky/static/js/main.c7c5e634.chunk.js"
+    "revision": "30a49360c722d42fa7d7",
+    "url": "/Simpsons-Clicky/static/js/main.30a49360.chunk.js"
   },
   {
     "revision": "82dafdb578ba27673946",
     "url": "/Simpsons-Clicky/static/js/1.82dafdb5.chunk.js"
   },
   {
-    "revision": "c7c5e6349adc34c9583c",
+    "revision": "30a49360c722d42fa7d7",
     "url": "/Simpsons-Clicky/static/css/main.9609d3b4.chunk.css"
   },
   {
-    "revision": "2aa00078c24e622a0ba01c15e3bd5987",
+    "revision": "3a4458a0eed8c598ea2335cdef0c653a",
     "url": "/Simpsons-Clicky/index.html"
   }
 ];
